6172d1583c5099cadc165b5fc85b606c5ac4b8da
mateo <mateoq152006@gmail.com>
2025-03-28 23:29:51 -0500

    se cre la interfas de tipo Agua

---
+++ POKEMON/src/autonoma/POKEMON/models/PokemonAgua.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package autonoma.POKEMON.models;
+
+/**
+ *
+ * @author mateo
+ */
+public interface PokemonAgua {
+    
+    
+    public abstract void atacarHidrobomba();
+    public abstract void atacarPistolaAgua() ;
+    public abstract void atacarBurbuja();
+    public abstract void atacarHidropulso();
+}
