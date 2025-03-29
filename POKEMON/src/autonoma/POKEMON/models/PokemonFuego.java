66513a30a85ad6e7fe2a2d858e07c0f904846b5f
mateo <mateoq152006@gmail.com>
2025-03-28 23:24:00 -0500

    se cre la interfas de tipo Fuego

---
+++ POKEMON/src/autonoma/POKEMON/models/PokemonFuego.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package autonoma.POKEMON.models;
+
+/**
+ *
+ * @author mateo
+ */
+public interface PokemonFuego {
+    
+    
+    public abstract void atacarPunioFuego();
+    public abstract void atacarAscuas() ;
+    public abstract void atacarLanzallamas();
+}
