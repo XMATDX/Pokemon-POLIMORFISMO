f4f433a4726487deda58998498d380e9d8bf4922
mateo <mateoq152006@gmail.com>
2025-03-29 21:01:36 -0500

    se cre la clase Squirtle

---
+++ POKEMON/src/autonoma/POKEMON/models/Squirtle.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package autonoma.POKEMON.models;
+
+/**
+ *
+ * @author mateo
+ */
+public class Squirtle extends Pokemon implements PokemonAgua{
+
+    public Squirtle(int numPOkedex, String nombre, double peso, int temporada) {
+        super(numPOkedex, nombre, peso, temporada);
+    }
+
+    @Override
+    public void atacarHidrobomba() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void atacarPistolaAgua() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void atacarBurbuja() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void atacarHidropulso() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
