fae71098d7ae1e268c85ff8f1109811dabefc6e4
mateo <mateoq152006@gmail.com>
2025-03-29 20:59:18 -0500

    se organiza el codigo de la clase Pikachu y se cre la clase de Charmander

---
+++ POKEMON/src/autonoma/POKEMON/models/Charmander.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package autonoma.POKEMON.models;
+
+/**
+ *
+ * @author mateo
+ */
+public class Charmander extends Pokemon implements PokemonFuego {
+
+    public Charmander(int numPOkedex, String nombre, double peso, int temporada) {
+        super(numPOkedex, nombre, peso, temporada);
+    }
+
+    @Override
+    public void atacarPunioFuego() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public void atacarAscuas() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public void atacarLanzallamas() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
--- POKEMON/src/autonoma/POKEMON/models/Pikachu.java
+++ POKEMON/src/autonoma/POKEMON/models/Pikachu.java
@@ -8,10 +8,30 @@ package autonoma.POKEMON.models;
  *
  * @author mateo
  */
-public class Pikachu extends Pokemon {
+public class Pikachu extends Pokemon implements PokemonElectrico{
     
     public Pikachu(int numPOkedex, String nombre, double peso, int temporada) {
         super(numPOkedex, nombre, peso, temporada);
+    }
+
+    @Override
+    public void atacarImpactrueno() {
+        System.out.println("Soy Pikachu y estoy atacando " + "con Impactrueno");
+    }
+
+    @Override
+    public void atacarPunioTrueno() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void atacarRayo() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void atacarRayoCarga() {
+        throw new UnsupportedOperationException("Not supported yet."); 
     }
     
 }
