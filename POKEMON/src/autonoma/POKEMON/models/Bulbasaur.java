de124c2bb234e1750ef46669e48eb99025925c48
Maribel Ceballos <maribelceballos@gmail.com>
2025-04-01 18:15:45 -0500

    se creo la clase bulbasaur

---
+++ POKEMON/src/autonoma/POKEMON/models/Bulbasaur.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package autonoma.POKEMON.models;
+
+/**
+ *
+ * @author marib
+ */
+public class Bulbasaur extends Pokemon implements PokemonPlanta {
+    
+
+    @Override
+    public String atacarParalizar() {
+        return ("Soy Bulbasaur y estoy atacando con atacar paralizar"); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public String atacarDrenaje() {
+        return ("Soy Bulbasaur y estoy atacando con atacar Drenaje"); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public String atacarHojaAfilada() {
+        return ("Soy Bulbasaur y estoy atacando con hoja afilada"); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public String atacarLatigoCepa() {
+        return ("Soy Bulbasaur y estoy atacando con atacar latigo cepa"); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    
+    
+    
+    
+}
